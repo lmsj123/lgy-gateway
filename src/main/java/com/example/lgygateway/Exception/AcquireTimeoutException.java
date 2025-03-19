8cf084cd041ac15e6ff1528144530b77cd456866
congyi.wan <congyi.wan@nio.com>
2025-03-19 10:27:51 +0800

    进一步完善加注释

--- src/main/java/com/example/lgygateway/Exception/AcquireTimeoutException.java
+++
@@ -1,7 +0,0 @@
-package com.example.lgygateway.Exception;
-
-public class AcquireTimeoutException extends Exception {
-    public AcquireTimeoutException(String rateLimitExceeded) {
-        super(rateLimitExceeded);
-    }
-}
--- src/main/java/com/example/lgygateway/filters/init/FiltersInit.java
+++ src/main/java/com/example/lgygateway/filters/init/FiltersInit.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.util.ServiceLoader;
 
 @Component
 @Data
@@ -16,34 +14,14 @@ public class FiltersInit {
     private FilterChain filterChain = new FilterChain();
     @PostConstruct
     public void init() {
-        // 动态加载指定路径下的过滤器类
-        loadFiltersFromPath();
+        // 使用 ServiceLoader 加载 SPI 实现类
+        loadFiltersBySPI();
     }
 
-    private void loadFiltersFromPath() {
-        try {
-            File filtersDir = new File("src/main/java/com/example/lgygateway/filters");
-            if (!filtersDir.exists() || !filtersDir.isDirectory()) {
-                throw new IllegalArgumentException("Invalid filters directory path: " + "target/classes/com/example/lgygateway/filters");
-            }
-
-            File[] files = filtersDir.listFiles((dir, name) -> name.endsWith(".class"));
-            if (files == null) {
-                return;
-            }
-            URL[] urls = {filtersDir.toURI().toURL()};
-            URLClassLoader classLoader = new URLClassLoader(urls);
-
-            for (File file : files) {
-                String className = file.getName().replace(".java", "");
-                Class clazz = classLoader.loadClass(className);
-                if (Filter.class.isAssignableFrom(clazz)) {
-                    Filter filter = (Filter) clazz.getDeclaredConstructor().newInstance();
-                    filterChain.addFilter(filter);
-                }
-            }
-        } catch (Exception e) {
-            e.fillInStackTrace();
+    private void loadFiltersBySPI() {
+        ServiceLoader<Filter> loader = ServiceLoader.load(Filter.class);
+        for (Filter filter : loader) {
+            filterChain.addFilter(filter);
         }
     }
 }
